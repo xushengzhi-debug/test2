404c7636db79e102fa498814413ccddc874a68e4
xushengzhi <283050583@qq.com>
2021-03-27 17:27:21 +0800

    Initial commit

---
+++ untitled/src/com/xu/hallword.java
@@ -0,0 +1,11 @@
+package com.xu;
+
+import java.util.Arrays;
+
+public class hallword {
+    public static void main(String[] args){
+        System.out.print("32324,word");
+        System.out.println("hallword.main");
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
